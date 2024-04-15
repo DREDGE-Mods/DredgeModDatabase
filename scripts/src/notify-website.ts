7efc5be515c637c7ba81845e0ac533dabd54d92a
Nick <nicholasconnors@hotmail.ca>
2024-04-15 13:16:16 -0400

    Update website when new mod is added

---
+++ scripts/src/notify-website.ts
@@ -0,0 +1,57 @@
+const core = require("@actions/core");
+const fetch = require("node-fetch");
+const fs = require("fs");
+const octo = require("octokit")
+import "./mod-info";
+
+async function run() {
+    core.info("Checking for added mods to refresh website");
+
+    var newDB = <DatabaseModInfo[]>JSON.parse(fs.readFileSync('./database/database.json','utf8'));
+    var oldDB = <DatabaseModInfo[]>JSON.parse(fs.readFileSync('./database/old_database.json','utf8'));
+
+    const oldDBDict: Record<string, DatabaseModInfo> = {};
+
+    for (const mod of oldDB) {
+        oldDBDict[mod.mod_guid] = mod;
+    }
+
+    for (const newMod of newDB) {
+        if (!(newMod.mod_guid in oldDBDict)) {
+            // New mod just released!
+            core.info(newMod.name + " was just added to the database!");
+            UpdateWebsite();
+            return;
+        }
+    }
+}
+
+async function UpdateWebsite() {
+    try {    
+        const octokit = new octo.Octokit({
+            auth: process.env["GITHUB_TOKEN"]
+        })
+
+        const response = await octokit.request('POST /repos/DREDGE-Mods/DredgeModsWebsite/dispatches', {
+            owner: 'DREDGE-Mods',
+            repo: 'DredgeModsWebsite',
+            event_type: 'mod_added',
+            client_payload: {
+              unit: false,
+              integration: true
+            },
+            headers: {
+              'X-GitHub-Api-Version': '2022-11-28'
+            }
+          })
+    
+        if (!response.ok) {
+            core.error(`API post response not ok. ${response.status}: ${response.statusText}`);
+        }
+    }
+    catch (error) {
+        core.error(`Failed to send Website notification: ${error}`);
+    }
+}
+
+run().catch((error) => core.setFailed("Workflow failed! " + error.message));
